202d1729ea29f8c8b282fd4b7b7c9069f8618fc8
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-09 06:24:21 +0200

    server

--- simple-client-server/server.py
+++ simple-client-server/server.py
@@ -1,1 +1,21 @@
-print("hi i am server")
+import socket
+
+HOST = "localhost" # 127.0.0.1
+PORT = 9090 # Connection Socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST,PORT))
+
+server.listen(5)
+
+print("Server Is Up !!!")
+
+while True:
+    communication_socket, address = server.accept()
+    print(f"Connected to {address}")
+    message = communication_socket.recv(1024).decode("utf-8") # every message is encoded
+    print(f"Message is: {message}")
+    
+    communication_socket.send("The message was received".encode("utf-8"))
+    
+    
