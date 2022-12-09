4b7f6d4d13b98d31e2b40eaca78ffc89d8297e90
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-09 06:24:28 +0200

    client

--- simple-client-server/client.py
+++ simple-client-server/client.py
@@ -1,1 +1,14 @@
-print("hello i am client")
+import socket
+
+SERVER_IP = "localhost" # "127.0.0.1"
+
+CONNECTION_PORT = 9090
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, CONNECTION_PORT))
+
+print("Client is Up !!!")
+
+message = input("Type a message: ")
+client.send(message.encode("utf-8"))
+print(client.recv(1024).decode("utf-8"))
